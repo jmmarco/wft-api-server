764c39b914f0641f32df256c5d7c4be792a46e2d
Juan Marco <jmmarco@gmail.com>
2020-04-08 12:38:36 -0300

    Shuffle entire array and return specific quantity

--- utils.js
+++ utils.js
@@ -7,5 +7,31 @@ module.exports = {
         translation: entry[1],
       };
     });
+  },
+  randomAcronyms: function (arr, quantity) {
+
+    if (isNaN(quantity)) {
+      quantity = arr.length;
+    } else {
+      quantity = parseInt(quantity)
+    }
+
+    // Fisher-Yates Algorithm Shuffle https://bost.ocks.org/mike/shuffle/
+      const shuffledArr = [...arr]
+      let m = arr.length, t, i
+      while (m) {
+        i = Math.floor(Math.random() * m--)
+        t = shuffledArr[m];
+        shuffledArr[m] = shuffledArr[i];
+        shuffledArr[i] = t;
+      }
+  
+      return shuffledArr.slice(0, quantity)
+  },
+  renderAcronyms: function(arr) {
+    let result = ''
+    arr.forEach(acr => result += `<p>${JSON.stringify(acr)}</p>`)
+    return result
   }
+
 };
