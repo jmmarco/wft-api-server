8074650a151fb0ba93d64b272deb033575e998cf
Juan Marco <jmmarco@gmail.com>
2020-04-09 17:05:23 -0300

    Use modern export syntax

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import express from 'express'
 import fs from 'fs'
-
-var utils = require("./utils");
+import { mapAndTransform } from './utils'
 
 const PORT = 3001;
 const app = express();
@@ -13,68 +12,6 @@ fs.readFile("acronyms.json", { encoding: "UTF8" }, (err, data) => {
   masterList = utils.mapAndTransform(JSON.parse(data));
 });
 
-// --- GET Routes ----
-
-// Complex search
-// /acronym?from=50&limit=10&search=:search
-app.get("/acronym?", (req, res) => {
-  const { from, limit, search } = req.query
-
-  if (parseInt(from) < 0 || parseInt(from) == 0 ) {
-    return res.json({ "error": 'invalid '})
-  }
-
-  const results = utils.paginatedResults(masterList, from, limit, search)
-
-  console.log('results is: ', results)
-
-  res
-  .status(200)
-  .json(results)
-  // .send(req.query)
-})
-
-// All acronyms
-app.get("/acronyms/", (req, res) => {
-  const buffer = utils.renderAcronyms(masterList);
-  res.send(buffer);
-});
-
-
-
-// Single acronym
-app.get("/acronym/:acronym", (req, res) => {
-  const { acronym } = req.params;
-
-  const acronymFound = masterList.find(
-    (item) => item.acronym === acronym.toUpperCase()
-  );
-
-  let response = null;
-
-  if (acronymFound) {
-    response = `acronym entered is ${JSON.stringify(acronymFound)}`;
-  } else {
-    response = `Nothing found for entry ${req.params.acronym}`;
-  }
-
-  res.status(200).json(acronymFound);
-});
-
-// Random amount of acronyms
-app.get("/random/:count?", (req, res) => {
-  const { count } = req.params;
-
-  const result = utils.randomAcronyms(masterList, count);
-  const buffer = utils.renderAcronyms(result);
-
-  res.status(200).send(`Count is ${count} and result is: ${buffer}`);
-});
-
-// Non matching routes return 404 by default
-app.get("*", (req, res) => {
-  res.sendStatus(404);
-});
 
 // Server conf
 const server = app.listen(PORT, () => {
--- utils.js
+++ utils.js
@@ -1,46 +1,49 @@
-module.exports = {
-  mapAndTransform: function (arr) {
-    return arr.map((acr) => {
-      const entry = Object.entries(acr)[0];
-      return {
-        acronym: entry[0],
-        definition: entry[1],
-      };
-    });
-  },
-  randomAcronyms: function (arr, quantity) {
-    if (isNaN(quantity)) {
-      quantity = arr.length;
-    } else {
-      quantity = parseInt(quantity);
-    }
+export function mapAndTransform(arr) {
+  return arr.map((acr) => {
+    const entry = Object.entries(acr)[0];
+    return {
+      acronym: entry[0],
+      definition: entry[1],
+    };
+  });
+}
 
-    // Fisher-Yates Algorithm Shuffle https://bost.ocks.org/mike/shuffle/
-    const shuffledArr = [...arr];
-    let m = arr.length,
-      t,
-      i;
-    while (m) {
-      i = Math.floor(Math.random() * m--);
-      t = shuffledArr[m];
-      shuffledArr[m] = shuffledArr[i];
-      shuffledArr[i] = t;
-    }
+export function randomAcronyms(arr, quantity) {
+  if (isNaN(quantity)) {
+    quantity = arr.length;
+  } else {
+    quantity = parseInt(quantity);
+  }
 
-    return shuffledArr.slice(0, quantity);
-  },
-  paginatedResults: function(arr, from, limit, search) {
-    console.log(from, limit, search )
+  // Fisher-Yates Algorithm Shuffle https://bost.ocks.org/mike/shuffle/
+  const shuffledArr = [...arr];
+  let m = arr.length,
+    t,
+    i;
+  while (m) {
+    i = Math.floor(Math.random() * m--);
+    t = shuffledArr[m];
+    shuffledArr[m] = shuffledArr[i];
+    shuffledArr[i] = t;
+  }
 
-    let filteredArr = arr.filter(acr => acr.acronym.indexOf(search.toUpperCase()) !== -1)
+  return shuffledArr.slice(0, quantity);
+}
 
-    console.log(filteredArr.length)
-    
-    return filteredArr
-  },
-  renderAcronyms: function (arr) {
-    let result = "";
-    arr.forEach((acr) => (result += `<p>${JSON.stringify(acr)}</p>`));
-    return result;
-  },
-};
+export function paginatedResults(arr, from, limit, search) {
+  console.log(from, limit, search);
+
+  let filteredArr = arr.filter(
+    (acr) => acr.acronym.indexOf(search.toUpperCase()) !== -1
+  );
+
+  console.log(filteredArr.length);
+
+  return filteredArr;
+}
+
+export function renderAcronyms(arr) {
+  let result = "";
+  arr.forEach((acr) => (result += `<p>${JSON.stringify(acr)}</p>`));
+  return result;
+}
