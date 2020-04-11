519817b027649af99506fc736c8953690f86b7f8
Juan Marco <jmmarco@gmail.com>
2020-04-10 23:08:15 -0300

    Remove unnecesary helper functions

--- utils.js
+++ utils.js
@@ -1,6 +1,4 @@
 export function mapAndTransform(arr) {
-
-  // arr.forEach(acr => console.log(Object.entries(acr)[0]))
   return arr.map((acr) => {
     const entry = Object.entries(acr)[0];
     return {
@@ -8,44 +6,4 @@ export function mapAndTransform(arr) {
       definition: entry[1],
     };
   });
-}
-
-export function randomAcronyms(arr, quantity) {
-  if (isNaN(quantity)) {
-    quantity = arr.length;
-  } else {
-    quantity = parseInt(quantity);
-  }
-
-  // Fisher-Yates Algorithm Shuffle https://bost.ocks.org/mike/shuffle/
-  const shuffledArr = [...arr];
-  let m = arr.length,
-    t,
-    i;
-  while (m) {
-    i = Math.floor(Math.random() * m--);
-    t = shuffledArr[m];
-    shuffledArr[m] = shuffledArr[i];
-    shuffledArr[i] = t;
-  }
-
-  return shuffledArr.slice(0, quantity);
-}
-
-export function paginatedResults(arr, from, limit, search) {
-  console.log(from, limit, search);
-
-  let filteredArr = arr.filter(
-    (acr) => acr.acronym.indexOf(search.toUpperCase()) !== -1
-  );
-
-  console.log(filteredArr.length);
-
-  return filteredArr;
-}
-
-export function renderAcronyms(arr) {
-  let result = "";
-  arr.forEach((acr) => (result += `<p>${JSON.stringify(acr)}</p>`));
-  return result;
 }
