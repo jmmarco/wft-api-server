0d07aac38a295a77446fa0149276457d7c539d97
Juan Marco <jmmarco@gmail.com>
2020-04-10 23:08:45 -0300

    Find single acronym

--- routes.js
+++ routes.js
@@ -3,6 +3,7 @@ import { Acronym } from "./index";
 import { Op } from "sequelize";
 
 const routes = (app) => {
+
   // All acronyms
   app.get("/acronyms/", (req, res) => {
     Acronym.findAll().then((acronyms) => {
@@ -10,7 +11,7 @@ const routes = (app) => {
     });
   });
 
-  //  Search with offset and limit
+  // Search with offset and limit
   app.get("/acronym?", (req, res, next) => {
     const { from, limit, search } = req.query;
 
@@ -34,29 +35,17 @@ const routes = (app) => {
   // Single acronym
   app.get("/acronym/:acronym", (req, res) => {
     const { acronym } = req.params;
-
-    const acronymFound = masterList.find(
-      (item) => item.acronym === acronym.toUpperCase()
-    );
-
-    let response = null;
-
-    if (acronymFound) {
-      response = `acronym entered is ${JSON.stringify(acronymFound)}`;
-    } else {
-      response = `Nothing found for entry ${req.params.acronym}`;
-    }
-
-    res.status(200).json(acronymFound);
+      Acronym.findOne({
+        where: { acronym: { [Op.like]: `%${acronym}` } },
+      }).then(results => {
+        res.json(results)
+      })
   });
 
   // Random amount of acronyms
   app.get("/random/:count?", (req, res) => {
     const { count } = req.params;
 
-    const result = randomAcronyms(masterList, count);
-
-    res.status(200).json(result);
   });
 
   // Non matching routes return 404 by default
