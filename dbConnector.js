74e324b7c05e19681f96b43ff8321084b3da7837
Juan Marco <jmmarco@gmail.com>
2020-04-13 17:12:00 -0300

    Abstract DB functionality

---
+++ dbConnector.js
@@ -0,0 +1,36 @@
+import Sequelize from "sequelize";
+import he from "he";
+
+// Instantiate the DB
+export const sequelize = new Sequelize("database", "admin", "admin", {
+  dialect: "sqlite",
+  storage: "./acronyms.sqlite",
+});
+
+// Authenticate with DB
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Connection has been established successfully.");
+  })
+  .catch((err) => {
+    console.error("Unable to connect to the database:", err);
+  });
+
+// Define the Model
+export const Acronym = sequelize.define("acronym", {
+  acronym: { type: Sequelize.STRING, allowNull: false },
+  definition: { type: Sequelize.STRING, allowNull: false },
+});
+
+// Sync the model with the database
+export function syncAndPopulate(arr) {
+  Acronym.sync({ force: true }).then(() => {
+    arr.forEach((item) => {
+      return Acronym.create({
+        acronym: item.acronym,
+        definition: he.decode(item.definition),
+      });
+    });
+  });
+}
