1e21774e1bc6a1fe8beb4187e11c944b3a6a70ed
Juan Marco <jmmarco@gmail.com>
2020-04-19 11:42:57 -0300

    Add capitalize method

--- utils.js
+++ utils.js
@@ -15,3 +15,7 @@ export function handleSequelizeError(err) {
   const { message } = ValidationErrorItem;
   return message;
 }
+
+export function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
