91909599083481cc2b7a6e34de19e85fb7c475ab
Juan Marco <jmmarco@gmail.com>
2020-04-21 12:39:19 -0300

    Switch back to Sqlite

--- dbConnector.js
+++ dbConnector.js
@@ -1,23 +1,11 @@
 import Sequelize from "sequelize";
 import he from "he";
 
-// Instantiate the DB
-export const sequelize = new Sequelize("acronyms", "postgres", "quilmes01", {
-  dialect: "postgres",
-  host: 'localhost'
+const sequelize = new Sequelize({
+  dialect: 'sqlite',
+  storage: 'acronyms.sqlite'
 });
 
-
-
-// Authenticate with DB
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log("Connection has been established successfully.");
-  })
-  .catch((err) => {
-    console.error("Unable to connect to the database:", err);
-  });
 
 // Define the Model
 export const Acronym = sequelize.define("acronym", {
