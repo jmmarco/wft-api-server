f270414cadc96862ae9c96bc330c4d2285e4849b
Juan Marco <jmmarco@gmail.com>
2020-04-23 14:43:29 -0300

    Fix sequelize instance

--- dbConnector.js
+++ dbConnector.js
@@ -2,22 +2,23 @@ import Sequelize from "sequelize";
 import he from "he";
 
 export const sequelize = new Sequelize({
-    database: process.env.DB_NAME,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    port: 5432,
-    dialect: "postgres",
-    dialectOptions: {
-      ssl: {
-        require: true,
-        // Ref.: https://github.com/brianc/node-postgres/issues/2009
-        rejectUnauthorized: false,
-      },
-      keepAlive: true,
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: 5432,
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      // Ref.: https://github.com/brianc/node-postgres/issues/2009
+      rejectUnauthorized: false,
     },
-    ssl: true,
+    keepAlive: true,
+  },
+  ssl: true,
 });
+}
 
 // Define the Model
 export const Acronym = sequelize.define("acronym", {
